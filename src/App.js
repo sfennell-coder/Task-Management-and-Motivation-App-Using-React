295c2af1d4f4fe77f9fd3844d6b81d6caeef7360
Scott Fennell <slfennell01@gmail.com>
2024-07-12 21:50:08 -0400

    App Component & additional features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import TaskGenerator from './TaskGenerator.js';
+import FancyText from './FancyText.js';
+import './App.css';
 
 function App() {
-  
+  return(
+    <div className='App'>
+      <FancyText title text='Task Management and Motivation App using React'/>
+      <TaskGenerator/>
+      <footer>
+        <p>©️ 2024 U86364709 All rights reserved</p>
+      </footer>
+    </div>
+  )
 }
 
 export default App;
