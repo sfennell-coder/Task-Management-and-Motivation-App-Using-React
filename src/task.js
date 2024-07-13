a3c1ba6f231ab954e05e6215c00596946d2b78df
Scott Fennell <slfennell01@gmail.com>
2024-07-12 21:03:19 -0400

    Add tasks in the array within the task.js file

--- src/task.js
+++ src/task.js
@@ -1,3 +1,8 @@
 export const tasks = [
-    {id: "", name: "", isCompleted:""}
+    {id:1, name: "Mow Yard", isCompleted: true},
+    {id:2, name: "Check and fix fences", isCompleted: true},
+    {id:3, name: "Turn off water", isCompleted: false},
+    {id:4, name: "Sharpen hand tools", isCompleted: false},
+    {id:5, name: "Check tire pressure", isCompleted: true},
+    {id:6, name: "Double check declination on compass", isCompleted: false}
 ]
